7743bd782ea5cf7ee337c58c36bdeb35c8ddbcbb
Mário Travassos <retchut@protonmail.ch>
2022-08-26 23:19:20 +0100

    refactor: page and routing changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,15 @@ import {
 } from "react-router-dom";
 
 import './App.css';
+import Showcase from './Pages/Showcase/Showcase.jsx';
 import Atlas from './Pages/Atlas/Atlas.jsx';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" exact element={<Atlas />} />
+        <Route path="/" exact element={<Atlas/>} />
+        <Route path="/showcase" exact element={<Showcase />} />
       </Routes>
     </>
   );
