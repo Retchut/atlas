5d86d3e5257875061b0201e0cc8283ade8acc0dc
Mário Travassos <retchut@protonmail.ch>
2022-08-26 22:39:40 +0100

    feat: fix pathology showcase height

--- client/src/Pages/Atlas/Atlas.jsx
+++ client/src/Pages/Atlas/Atlas.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Pathology from '../../Components/Pathology/Pathology.jsx'
+import Pathology from '../../Components/Pathology/Pathology.jsx';
+import './Atlas.css';
 
 function Atlas(){
     const [state, setState] = useState({ apiResponse : [], currentPathologyID : null });
@@ -19,12 +20,12 @@ function Atlas(){
 
     function displayPathology(){
         if(state.currentPathologyID === null){
-            return (<h1>Please select a pathology on the menu to the left</h1>);
+            return (<h1 className="my-5 text-5xl text-center">Please select an image on the menu to the left</h1>);
         }
 
         const pathology = state.apiResponse.find((item) => item._id === state.currentPathologyID);
         if(typeof pathology == "undefined"){
-            return (<h1>Error fetching pathology.</h1>);
+            return (<h1 className="my-5 text-5xl text-center">Error fetching pathology.</h1>);
         }
         else{
             return (<Pathology pathologyData={pathology}></Pathology>);
@@ -32,27 +33,25 @@ function Atlas(){
     }
 
     return(
-        <div className="atlas-page">
-            <div className="h-screen w-screen flex justify-center items-center">
-                <div className="gap-2 w-2/3 h-screen3/4 auto-cols-max flex">
-                    {/* pathology selector */}
-                    <div className="bg-theme2-blue-light rounded-lg overflow-auto min-w-max w-fit flex justify-center">
-                        <div className="">
-                            {state.apiResponse.map((pathology, index) => (
-                                <img
-                                    className="max-w-pathology-thumbnail m-4 rounded-lg cursor-pointer"
-                                    src={process.env.REACT_APP_SERVER_STORAGE_DIR + pathology.imageHash}
-                                    alt={pathology.name + '-thumb'}
-                                    onClick={() => handlePathologyClick(pathology._id)}
-                                    key={'pathology-thumb-' + index}>
-                                </img>))
-                            }
-                        </div>
+        <div className="h-screen w-screen flex justify-center items-center">
+            <div className="gap-2 w-2/3 h-screen3/4 auto-cols-max flex">
+                {/* pathology selector */}
+                <div className="atlas-section min-w-max w-fit flex justify-center">
+                    <div>
+                        {state.apiResponse.map((pathology, index) => (
+                            <img
+                                className="max-w-pathology-thumbnail m-4 rounded-lg cursor-pointer"
+                                src={process.env.REACT_APP_SERVER_STORAGE_DIR + pathology.imageHash}
+                                alt={pathology.name + '-thumb'}
+                                onClick={() => handlePathologyClick(pathology._id)}
+                                key={'pathology-thumb-' + index}>
+                            </img>))
+                        }
                     </div>
-                    {/* pathology showcase */}
-                    <div className="bg-theme2-blue-light rounded-lg overflow-auto grow"> {/*min-w-max*/}
-                        {displayPathology()}
-                    </div>
+                </div>
+                {/* pathology showcase */}
+                <div className="atlas-section h-fit max-h-full grow">
+                    {displayPathology()}
                 </div>
             </div>
         </div>
