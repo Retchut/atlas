2ce752f41ccb768810f39b86810b406947239f94
Mário Travassos <retchut@protonmail.ch>
2022-09-23 12:19:26 +0100

    feat: add atlas menu data

---
+++ client/src/Data/MenuData.js
@@ -0,0 +1,114 @@
+export const MenuData = [
+    {
+        'Adaptação e lesão celular sub-letal' : [
+            'Tumefação celular',
+            'Alteração vacuolar',
+            'Alteração ou acumulação hidrópica',
+            'Alteração balonizante',
+            'Inclusões hialinas',
+            'Inclusões Pigmentares',
+            'Inclusões virais',
+            'Inclusões Cristalinas'
+        ]
+    },
+    {
+        'Alterações metabólicas e acumulações' : [
+            'Alteração do metabolismo dos Lípiidos',
+            'Alteração do metabolismo dos glúcidos',
+            'Alteração do metabolismo dos mucopolissacáridos',
+            {
+                'Alteração do metabolismo das proteínas' : [
+                    'Alteração da Queratina',
+                    'Alteração do Colagénio',
+                    {
+                        'Alteração Hialina' : [
+                            'Amiloidose'
+                        ]
+                    }
+                ]
+            },
+            'Alteração do metabolismo das Nucleoproteínas',
+            'Alteração do metabolismo do Cálcio',
+            {
+                'Pigmentos' : [
+                    {
+                        'Pigmentos Exógenos' : [
+                            'Carvão e Sílica'
+                        ],
+                    },
+                    {
+                        'Pigmentos Endógenos' : [
+                            'Melanina',
+                            'Hemossiderina',
+                            'Pigmento Biliar',
+                            'Pigmentos Lipogénicos'
+                        ],
+                    }
+                ]
+                
+            },
+            'Cálculos e Pseudocálculos'
+        ],
+    },
+    {
+        'Necrose Celular e Tecidular' : [
+            'Necrose de Coagulação',
+            'Necrose de Liquefação',
+            'Necrose de Caseificação ',
+            'Necrose de Superfície - Erosão e Úlcera',
+            'Gangrena'
+        ],
+    },
+    {
+        'Inflamação' : [
+            {
+                'Inflamação Aguda' : [
+                    'Inflamação serosa',
+                    'Inflamação purulenta',
+                    'Inflamação catarral',
+                    {
+                        'Inflamação fibrinosa' : [
+                            'Inflamação pseudomembranosa'
+                        ],
+                    }
+                ],
+            },
+            'Inflamação Crónica'
+        ],
+    },
+    {
+        'Reconstrução tecidular' : [
+            'Regeneração',
+            {
+                'Reparação' : [
+                    'Tecido de Granulação e Cicatriz ',
+                    'Problemas na Cicatrização'
+                ]
+            }
+        ],
+    },
+    {
+        'Alterações circulatórias' : [
+            'Congestão e Hemorragia',
+            'Trombose',
+            'Embolia',
+            'Enfarte',
+            'Edema'
+        ],
+    },
+    {
+        'Alterações do crescimento e diferenciação' : [
+            'Congénitas',
+            {
+                'Adquiridas/adaptativas' : [
+                    'Hipertrofia e atrofia',
+                    'Hiperplasia e involução',
+                    'Metaplasia ',
+                    'Displasia'
+                ]
+            }
+        ],
+    },
+    'Neoplasias (tumores)',
+    'Alterações post mortem e Fenómenos cadavéricos'
+]
