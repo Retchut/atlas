37a3e1fdd78ad36b80ddebc3396bb4c26389ceb5
Mário Travassos <retchut@protonmail.ch>
2022-12-06 11:18:31 +0000

    feat: add deploy scripts

--- deployPrep.sh
+++ deployPrep.sh
@@ -1,8 +1,8 @@
-echo 'Copying .envProd into .env'
-cd server
-cp .envProd .env
-cd ..
-cd client
-cp .envProd .env
+echo 'Copying production enviromentf files into the respective .env files'
+cp client/.envProd client/.env
+cp server/.envProd server/.env
 echo 'Building frontend application'
-npm run build
+cd client && npm run build
+cd build/ && flyctl launch
+echo 'Building backend application'
+cd server && flyctl launch --dockerfile Dockerfile-prod
