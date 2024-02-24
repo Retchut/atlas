a37373cfe8b193b1b02afdd28c16ff9533d0beef
Mário Travassos <retchut@protonmail.ch>
2024-02-24 11:34:58 +0000

    feat: add simple footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home.jsx";
 import Atlas from "./Pages/Atlas/Atlas.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/atlas" exact element={<Atlas />} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="fixed -bottom-2 w-full flex justify-center">
+      <footer className="w-fit atlas-section pt-1 pb-2 px-2">
+        © Fernanda Seixas, Maria dos Anjos 2024
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
